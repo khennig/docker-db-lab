eec82b1f46fd27cdd67e5fc80908567433773d96
Kai Hennig <khennig@pobox.com>
2024-02-10 10:03:39 +0100

    Wait for database service to accepting connections

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,18 @@ POSTGRES_USER=myuser
 POSTGRES_PASSWORD=mypass
 POSTGRES_URL=jdbc:postgresql://localhost:5432/$POSTGRES_DB
 
+wait_for_postgresql() {
+  TRIES=0
+  while ! docker exec docker-db-lab pg_isready; do
+    TRIES=$((TRIES + 1))
+    if [ "$TRIES" -ge "10" ]; then
+      echo "Stop waiting for PostgreSQL to accept connections"
+      exit 1
+    fi
+    sleep 1
+  done
+}
+
 docker build -t de.erik.lab/docker-db-lab .
 docker rm -f docker-db-lab || true
 docker run -d -p 5432:5432  \
@@ -15,6 +27,9 @@ docker run -d -p 5432:5432  \
   --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
   --name docker-db-lab \
   de.erik.lab/docker-db-lab
+
+echo "Waiting for PostgreSQL to accept conntections ..."
+wait_for_postgresql
 
 mvn clean compile flyway:migrate \
   -Dflyway.url=$POSTGRES_URL \
